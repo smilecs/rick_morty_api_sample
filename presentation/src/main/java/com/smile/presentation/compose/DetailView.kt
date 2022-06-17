b4f0804e3a538623ed6b359d424f10184ce4e486
egbai mmumene <mumene@gmail.com>
2022-06-17 13:46:18 +0200

    compose

---
+++ presentation/src/main/java/com/smile/presentation/compose/DetailView.kt
@@ -0,0 +1,25 @@
+package com.smile.presentation.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.smile.presentation.uimodel.CharacterUI
+
+@Composable
+fun DetailView(characterUI: CharacterUI) {
+    //Column(modifier = Modifier.fillMaxWidth()) {
+        Text(characterUI.name, modifier = Modifier.paddingFromBaseline(top = 8.dp))
+   // }
+}
+
+@Preview
+@Composable
+fun showDetailView(){
+    DetailView(CharacterUI(1, "Smile", "Immortal", "", "Male", "", null))
+}
